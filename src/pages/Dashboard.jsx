cf6ec47335a1ea4d1b653b460d22ea9abbd1b4d3
Alain Justilien <alainjustilien@gmail.com>
2025-11-30 21:41:16 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -90,6 +90,7 @@ export default function Dashboard() {
         </motion.div>
 
         <StatsOverview salespeople={salespeople} />
+        <DownloadStats />
 
         {isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
