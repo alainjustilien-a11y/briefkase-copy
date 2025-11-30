19cae283a2b9d032694046f43fb120a3e43f7544
Alain Justilien <alainjustilien@gmail.com>
2025-11-30 18:41:21 +0000

    File changes

--- src/pages/PortfolioSummary.jsx
+++ src/pages/PortfolioSummary.jsx
@@ -23,9 +23,10 @@ export default function PortfolioSummary() {
       }
       
       try {
-        const people = await base44.entities.Salesperson.filter({ id: personId });
-        if (people && people.length > 0) {
-          setPerson(people[0]);
+        const allPeople = await base44.entities.Salesperson.list();
+        const found = allPeople.find(p => p.id === personId);
+        if (found) {
+          setPerson(found);
         } else {
           setError('Portfolio not found');
         }
